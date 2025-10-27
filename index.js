c3b40aff2d6ea7da3715ef2a037d926bbc03c458
Julian R <alobuenastarde@gmail.com>
2025-10-27 12:41:56 -0500

    added inital set up for the api

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
